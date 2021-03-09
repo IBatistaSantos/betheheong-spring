498559cab8bed5e55fe5c0327a19ac05aa73b12c
Israel Batista <israelbatiista19@gmail.com>
2021-03-09 11:07:55 -0300

    Created Entity ONG

---
+++ src/main/java/br/com/bethehero/bethehero/domain/entities/Ong.java
@@ -0,0 +1,16 @@
+package br.com.bethehero.bethehero.domain.entities;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Ong {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String whatsapp;
+    private String city;
+    private String UF;
+}
