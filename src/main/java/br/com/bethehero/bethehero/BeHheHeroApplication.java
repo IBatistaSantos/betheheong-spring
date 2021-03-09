ea98061cb9aab699d22b561815d2cf52c3867e5d
Israel Batista <israelbatiista19@gmail.com>
2021-03-09 10:19:12 -0300

    First commit

---
+++ src/main/java/br/com/bethehero/bethehero/BeHheHeroApplication.java
@@ -0,0 +1,13 @@
+package br.com.bethehero.bethehero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeHheHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeHheHeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bethehero/bethehero/BeHheHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bethehero.bethehero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeHheHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
